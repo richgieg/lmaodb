2dc0be7f9ad655ca2a2bca0d94be46e947984543
Richard Gieg <richgieg@users.noreply.github.com>
2016-04-19 02:57:28 -0700

    Support more types in sort functionality

--- sortable.go
+++ sortable.go
@@ -23,12 +23,32 @@ func (s *sortable) Less(i, j int) (result bool) {
 	x := s.sliceValue.Index(i).FieldByName(s.field).Interface()
 	y := s.sliceValue.Index(j).FieldByName(s.field).Interface()
 	switch x.(type) {
+	case byte:
+		result = x.(byte) < y.(byte)
+	case float32:
+		result = x.(float32) < y.(float32)
+	case float64:
+		result = x.(float64) < y.(float64)
 	case int:
 		result = x.(int) < y.(int)
+	case int8:
+		result = x.(int8) < y.(int8)
+	case int16:
+		result = x.(int16) < y.(int16)
+	case int32:
+		result = x.(int32) < y.(int32)
 	case int64:
 		result = x.(int64) < y.(int64)
 	case string:
 		result = x.(string) < y.(string)
+	case uint:
+		result = x.(uint) < y.(uint)
+	case uint16:
+		result = x.(uint16) < y.(uint16)
+	case uint32:
+		result = x.(uint32) < y.(uint32)
+	case uint64:
+		result = x.(uint64) < y.(uint64)
 	}
 	return
 }
